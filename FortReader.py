1b171c73ced78195a9de09025dbce6e6130819be
Pranav Sai <p_sai@coloradocollege.edu>
2024-03-19 22:15:39 +0000

    Combined elevation graph

--- FortReader.py
+++ FortReader.py
@@ -362,7 +362,7 @@ class PostReader:
         stationToNodeDistancesDict = {}
 
         # Set to true to recreate station to node distances calculations dictionary
-        initializeStationToNodeDistancesDict = False
+        initializeStationToNodeDistancesDict = True
         if(initializeStationToNodeDistancesDict):
             for stationKey in stationsDict["NOS"].keys():
                 stationToNodeDistancesDict[stationKey] = {}
@@ -396,7 +396,7 @@ class PostReader:
   
         nodes = {"NOS": {}}
     
-        initializeNodesDict = False
+        initializeNodesDict = True
         if(initializeNodesDict):
             for stationKey in stationToNodeDistancesDict.keys():
                 stationToNodeDistanceDict = stationToNodeDistancesDict[stationKey]
@@ -412,7 +412,7 @@ class PostReader:
         with open(self.POST_NODES_FILE) as outfile:
             nodes = json.load(outfile)
 
-        initializeWindDataDict = False
+        initializeWindDataDict = True
         if(initializeWindDataDict):
             windData = {}
             for nodeIndex in nodes["NOS"].keys():
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 ./richamp_scale_and_subset_post_init.sh
-sbatch runmu.sh
 ./richamp_scale_and_subset.sh
