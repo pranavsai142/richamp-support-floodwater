fb4a521c9c7bc01064a786cb7492cb15257cb71d
Pranav Sai <p_sai@coloradocollege.edu>
2024-06-24 05:51:09 +0000

    Make script exit gracefully
    Fix low res post threshold and make script exit gracefully

--- run.sh
+++ run.sh
@@ -1,2 +1,17 @@
+rm -f *.start *.finish *.submit
 $POSTHOME/richamp_scale_and_subset_post_init.sh
-# $POSTHOME/richamp_scale_and_subset.sh
+while ! test -e richamp_scale_and_subset_post_init.scr.start
+do
+    sleep 5
+done
+job_string=$(cat richamp_scale_and_subset_post_init.scr.start)
+search=jobid
+left_job_string=${job_string%%$search*}
+jobid_lit_idx=${#left_job_string}
+jobid_idx=$(($jobid_lit_idx+10))
+jobid_endidx=$((${#job_string}-$jobid_idx-1))
+jobid=${job_string:$jobid_idx:$jobid_endidx}
+while squeue -j $jobid | grep -q $jobid >/dev/null 2>&1
+do
+    sleep 10
+done
