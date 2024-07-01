b22de6e2368c39cfa6fe0a3f0dcd44a535bf76e6
Pranav Sai <pranav.sai@uri.edu>
2024-07-01 11:24:18 -0500

    Fix parametric wind generation

--- trackll_2num.m
+++ trackll_2num.m
@@ -3,26 +3,30 @@ function [out]=trackll_2num(IN)
 A=IN;
 sz=size(A);
 
-c1=cell2mat(A(:,1));
-c2=cell2mat(A(:,2));
+c1=(A(:,1));
+c2=(A(:,2));
 
 for i=1:sz(1)
-Ao(i,1)=(str2double(c1(i,1:3)))/10;
-    Aon(i,1)=c1(i,4);    
-        if(Aon(i,1)=='S')
-            Ao(i,1)=-Ao(i,1);
-        end
+    latstr=c1(i);
+    latstrlength=strlength(latstr)
+    latstr=char(latstr)
+    lat=latstr(1,1:end-1);
+    Ao(i, 1)=(str2double(lat))/10;
+    if(contains(latstr,'S'))
+        Ao(i,1)=-Ao(i,1);
+    end
 
-            cc=length(c2(i,:));
-        
-    Ao(i,2)=(str2double(c2(i,1:(cc-1))))/(.001*10^cc);
+    lonstr=c2(i);
+    lonstr=char(lonstr)
+    lon=lonstr(1,1:end-1)    
+    Ao(i,2)=(str2double(lon))/10;
 
-    Aoe(i,1)=c2(i,cc);
-           if(Aoe(i,1)=='W')
-            Ao(i,2)=-Ao(i,2);
-           end
+    if(contains(lonstr,'W'))
+        Ao(i,2)=-Ao(i,2);
+    end
         
 end
 
 out=Ao;
+disp(out);
 end
