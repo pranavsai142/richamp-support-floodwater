b78e4c9d352f71a71ed6d9b321c6502c6cda7a64
Pranav Sai <pranavxd142@gmail.com>
2025-06-16 07:22:10 -0700

    Adjust wave points

--- generateNormalPoints.py
+++ generateNormalPoints.py
@@ -17,17 +17,17 @@ DEEPLINE_DISTANCES = [
 # Station-specific deepline distances with depths
 DEEPLINE_DISTANCES_1 = [
     {"distance": 705, "depth": "7m"},   # Napatree1 (runup_id: 10)
-    {"distance": 2350, "depth": "20m"},
+    {"distance": 2325, "depth": "20m"},
     {"distance": 4000, "depth": "40m"}
 ]
 DEEPLINE_DISTANCES_2 = [
-    {"distance": 400, "depth": "7m"},   # Napatree2 (runup_id: 20)
-    {"distance": 2300, "depth": "20m"},
+    {"distance": 410, "depth": "7m"},   # Napatree2 (runup_id: 20)
+    {"distance": 2275, "depth": "20m"},
     {"distance": 4000, "depth": "40m"},
 ]
 DEEPLINE_DISTANCES_3 = [
     {"distance": 360, "depth": "7m"}, # Napatree3 (runup_id: 30)
-    {"distance": 2300, "depth": "20m"},
+    {"distance": 2250, "depth": "20m"},
     {"distance": 4000, "depth": "40m"},
 ]
 DEEPLINE_DISTANCES_4 = [
@@ -37,7 +37,7 @@ DEEPLINE_DISTANCES_4 = [
 ]
 DEEPLINE_DISTANCES_5 = [
     {"distance": 545, "depth": "7m"},   # Napatree5 (runup_id: 50)
-    {"distance": 2150, "depth": "20m"},
+    {"distance": 2200, "depth": "20m"},
     {"distance": 4000, "depth": "40m"}
 ]
 
