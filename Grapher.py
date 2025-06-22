b7e8e500d2399c3acc3a6d61cf5a0dcb81c3b06a
Pranav Sai <pranavxd142@gmail.com>
2025-06-22 13:30:10 -0700

    Graph swash for Dec 2023

--- Grapher.py
+++ Grapher.py
@@ -21,7 +21,7 @@ DEPTH_LINE_7M = -7.0
 DEPTH_LINE_20M = -20.0
 DISTANCE_LINE_9000M = 9000.0
 
-GRAPH_SWASH = False
+GRAPH_SWASH = True
 
 plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
 plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
