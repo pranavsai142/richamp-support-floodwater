f810d22c3fd995ff41ff62db20f0056e61d39c19
Pranav Sai <pranavxd142@gmail.com>
2025-06-22 15:22:36 -0700

    Fix daily average slope calculation

--- GetRunup.py
+++ GetRunup.py
@@ -489,7 +489,7 @@ class GetRunup:
 #                 averageSlope = math.atan((waterlineElevation - adjacentWaterlineElevation) / waterlineDistance)
 #                 averageSlopes.append(averageSlope)
                 
-            if(CALCULATE_DAILY_AVERAGE_SLOPE):
+            if(not CALCULATE_DAILY_AVERAGE_SLOPE):
                 startOfDayTime = None
                 maxWaterlineKey = None
                 minWaterlineKey = None
@@ -573,7 +573,7 @@ class GetRunup:
                 
 #                 Now I need to calculate the averageSlope using the waterlineKey point
 
-                if(CALCULATE_DAILY_AVERAGE_SLOPE):
+                if(not CALCULATE_DAILY_AVERAGE_SLOPE):
                     averageSlope = averageSlopes[index]
                 else:
                     waterlineDistance = haversine.haversine(waterlineCoordinates, adjacentWaterlineCoordinates) * 1000
