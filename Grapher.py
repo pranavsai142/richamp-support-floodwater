5b46225103326abaafd3ece61eef5c03b835c6c8
Pranav Sai <pranavxd142@gmail.com>
2025-06-25 20:32:03 -0700

    Correctly graph dune height for 2023

--- Grapher.py
+++ Grapher.py
@@ -2432,9 +2432,9 @@ class Grapher:
             for height in unique_heights:
                 if transect >= 2: #For 2023, 5 for 2022
 #                 if transect >= 5:
-                    ax.axhline(y=height, linestyle='--', color='red', label=f'Runup Height {height:.2f}m' if height == list(unique_heights)[0] else None)
+                    ax.axhline(y=height, linestyle='--', color='red', label=f'Dune Height {height:.2f}m' if height == list(unique_heights)[0] else None)
                 else:
-                    ax.axhline(y=height, linestyle='--', color='grey', label=f'Runup Height {height:.2f}m' if height == list(unique_heights)[0] else None)
+                    ax.axhline(y=height, linestyle='--', color='grey', label=f'Dune Height {height:.2f}m' if height == list(unique_heights)[0] else None)
         
             ax.legend(loc="upper left", fontsize=10)
             ax.format_xdata = mdates.DateFormatter('%d')
