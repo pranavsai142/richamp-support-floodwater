6d968adcb749dde1a18fb62679bef16e090710a4
Pranav Sai <pranavxd142@gmail.com>
2025-07-02 10:03:53 -0700

    Import mcolors

--- Grapher.py
+++ Grapher.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import matplotlib.dates as mdates
+import matplotlib.colors as mcolors
 from matplotlib.cm import ScalarMappable
 from matplotlib.tri import Triangulation
 from datetime import datetime, timezone
