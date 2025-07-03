7edd8391a2191fadcefc36a6e970f2ec8933a1e9
Pranav Sai <pranavxd142@gmail.com>
2025-07-03 09:41:14 -0700

    Uncomment reader

--- generateGraphs.py
+++ generateGraphs.py
@@ -476,7 +476,7 @@ def main():
 
         ADCIRC_WATER_DATA_FILE = water_temp_directory + "adcirc_water_data_file" + ".json"
 
-#         (waterStartDateObject, waterEndDateObject) = Fort63Reader(ADCIRC_WATER_FILE=ADCIRC_WATER_FILE, STATIONS_FILE=STATIONS_FILE, ADCIRC_WATER_DATA_FILE=ADCIRC_WATER_DATA_FILE, BACKGROUND_AXIS=backgroundAxis).generateWindDataForStations()
+        (waterStartDateObject, waterEndDateObject) = Fort63Reader(ADCIRC_WATER_FILE=ADCIRC_WATER_FILE, STATIONS_FILE=STATIONS_FILE, ADCIRC_WATER_DATA_FILE=ADCIRC_WATER_DATA_FILE, BACKGROUND_AXIS=backgroundAxis).generateWindDataForStations()
 #         waterStartDateObject = datetime.datetime(year=2022, month=12, day=20, hour=0, tzinfo=datetime.timezone.utc)
 #         waterEndDateObject = datetime.datetime(year=2022, month=12, day=25, hour=0, tzinfo=datetime.timezone.utc)
         dataToGraph["WATER"] = ADCIRC_WATER_DATA_FILE
