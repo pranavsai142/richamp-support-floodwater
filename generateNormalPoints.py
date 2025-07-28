08930b1aef04d7a03d375086bfecb2e7951c0709
Pranav Sai <pranavxd142@gmail.com>
2025-07-27 23:39:54 -0700

    Runup 2023 hard

--- generateNormalPoints.py
+++ generateNormalPoints.py
@@ -438,44 +438,44 @@ def datetime_to_timestamp(dt_str):
 # Station-specific dune heights with GMT datetime strings
 
 # 2023
-# DUNE_HEIGHTS_1 = [
-#     {"datetime": "2023-12-13 04:00:00", "height": 6.04},
-#     {"datetime": "2023-12-19 04:00:00", "height": 6.08}
-# ]
-# DUNE_HEIGHTS_2 = [
-#     {"datetime": "2023-12-13 04:00:00", "height": 4.81},
-#     {"datetime": "2023-12-19 04:00:00", "height": 4.82}
-# ]
-# DUNE_HEIGHTS_3 = [
-#     {"datetime": "2023-12-13 04:00:00", "height": 4.02},
-#     {"datetime": "2023-12-19 04:00:00", "height": 4.06}
-# ]
-# DUNE_HEIGHTS_4 = [
-#     {"datetime": "2023-12-13 04:00:00", "height": 3.89},
-#     {"datetime": "2023-12-19 04:00:00", "height": 3.92}
-# ]
-# DUNE_HEIGHTS_5 = [
-#     {"datetime": "2023-12-13 04:00:00", "height": 3.32},
-#     {"datetime": "2023-12-19 04:00:00", "height": 3.22}
-# ]
+DUNE_HEIGHTS_1 = [
+    {"datetime": "2023-12-13 04:00:00", "height": 6.04},
+    {"datetime": "2023-12-19 04:00:00", "height": 6.08}
+]
+DUNE_HEIGHTS_2 = [
+    {"datetime": "2023-12-13 04:00:00", "height": 4.81},
+    {"datetime": "2023-12-19 04:00:00", "height": 4.82}
+]
+DUNE_HEIGHTS_3 = [
+    {"datetime": "2023-12-13 04:00:00", "height": 4.02},
+    {"datetime": "2023-12-19 04:00:00", "height": 4.06}
+]
+DUNE_HEIGHTS_4 = [
+    {"datetime": "2023-12-13 04:00:00", "height": 3.89},
+    {"datetime": "2023-12-19 04:00:00", "height": 3.92}
+]
+DUNE_HEIGHTS_5 = [
+    {"datetime": "2023-12-13 04:00:00", "height": 3.32},
+    {"datetime": "2023-12-19 04:00:00", "height": 3.22}
+]
 
 
 # 2022
-DUNE_HEIGHTS_1 = [
-    {"datetime": "2022-12-20 04:00:00", "height": 3.82}
-]
-DUNE_HEIGHTS_2 = [
-    {"datetime": "2022-12-20 04:00:00", "height": 3.37}
-]
-DUNE_HEIGHTS_3 = [
-    {"datetime": "2022-12-20 04:00:00", "height": 3.40}
-]
-DUNE_HEIGHTS_4 = [
-    {"datetime": "2022-12-20 04:00:00", "height": 3.84}
-]
-DUNE_HEIGHTS_5 = [
-    {"datetime": "2022-12-20 04:00:00", "height": 3.11}
-]
+# DUNE_HEIGHTS_1 = [
+#     {"datetime": "2022-12-20 04:00:00", "height": 3.82}
+# ]
+# DUNE_HEIGHTS_2 = [
+#     {"datetime": "2022-12-20 04:00:00", "height": 3.37}
+# ]
+# DUNE_HEIGHTS_3 = [
+#     {"datetime": "2022-12-20 04:00:00", "height": 3.40}
+# ]
+# DUNE_HEIGHTS_4 = [
+#     {"datetime": "2022-12-20 04:00:00", "height": 3.84}
+# ]
+# DUNE_HEIGHTS_5 = [
+#     {"datetime": "2022-12-20 04:00:00", "height": 3.11}
+# ]
 
 # Convert datetime strings to timestamps for JSON output
 DUNE_HEIGHTS_1 = [{"timestamp": datetime_to_timestamp(h["datetime"]), "height": h["height"]} for h in DUNE_HEIGHTS_1]
--- localGenerator.sh
+++ localGenerator.sh
@@ -152,19 +152,19 @@
 # --wavepwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/swan_TPS.63.nc \
 #  --tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/temp/ --backgroundChoice NAPATREE
 
-python generateGraphs.py --stations NAPATREE_NORMAL_STATIONS.json \
---wavesExists true \
---waverad /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/rads.64.nc \
---waveswh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/swan_HS.63.nc \
---wavemwd /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/swan_DIR.63.nc \
---wavemwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/swan_TMM10.63.nc \
---wavepwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/swan_TPS.63.nc \
---waterExists true --water /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/fort.63.nc \
---stillwaterExists true --stillwater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_wind/fort.63.nc \
---tidewaterExists true --tidewater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_tides/fort.63.nc \
---meshExists true --mesh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/fort.14 \
---generateRunup true \
---tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/dec2022_runup_temp/ --backgroundChoice NAPATREE --graphDirectory Runup2022Hard/
+# python generateGraphs.py --stations NAPATREE_NORMAL_STATIONS.json \
+# --wavesExists true \
+# --waverad /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/rads.64.nc \
+# --waveswh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/swan_HS.63.nc \
+# --wavemwd /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/swan_DIR.63.nc \
+# --wavemwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/swan_TMM10.63.nc \
+# --wavepwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/swan_TPS.63.nc \
+# --waterExists true --water /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/fort.63.nc \
+# --stillwaterExists true --stillwater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_wind/fort.63.nc \
+# --tidewaterExists true --tidewater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_tides/fort.63.nc \
+# --meshExists true --mesh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec222022RunupRun/forecast_RI_track_waves/fort.14 \
+# --generateRunup true \
+# --tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/dec2022_runup_temp/ --backgroundChoice NAPATREE --graphDirectory Runup2022Hard/
 
 # Graph 2022 Transect
 # python generateGraphs.py --stations NAPATREE_NORMAL_TRANSECT_STATIONS.json \
@@ -244,19 +244,19 @@ python generateGraphs.py --stations NAPATREE_NORMAL_STATIONS.json \
 # Dec 2023 Figures
 
 #  Mesh Map, and Runup
-# python generateGraphs.py --stations NAPATREE_NORMAL_STATIONS.json \
-# --wavesExists true \
-# --waverad /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/rads.64.nc \
-# --waveswh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/swan_HS.63.nc \
-# --wavemwd /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/swan_DIR.63.nc \
-# --wavemwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_master_build/swan_TMM10.63.nc \
-# --wavepwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/swan_TPS.63.nc \
-# --waterExists true --water /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/fort.63.nc \
-# --stillwaterExists true --stillwater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_wind/fort.63.nc \
-# --tidewaterExists true --tidewater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_tides/fort.63.nc \
-# --meshExists true --mesh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/fort.14 \
-# --generateRunup true \
-# --tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/dec2023_runup_temp/ --backgroundChoice NAPATREE --graphDirectory Runup2023Average/
+python generateGraphs.py --stations NAPATREE_NORMAL_STATIONS.json \
+--wavesExists true \
+--waverad /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/rads.64.nc \
+--waveswh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/swan_HS.63.nc \
+--wavemwd /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/swan_DIR.63.nc \
+--wavemwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_master_build/swan_TMM10.63.nc \
+--wavepwp /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/swan_TPS.63.nc \
+--waterExists true --water /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/fort.63.nc \
+--stillwaterExists true --stillwater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_wind/fort.63.nc \
+--tidewaterExists true --tidewater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_tides/fort.63.nc \
+--meshExists true --mesh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/fort.14 \
+--generateRunup true \
+--tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/dec2023_runup_temp/ --backgroundChoice NAPATREE --graphDirectory Runup2023Hard/
 
 # Water swath
 # python generateGraphs.py --stations OBS_STATIONS.json --obsExists true --waterExists true --water /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/fort.63.nc --stillwaterExists true --stillwater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_wind/fort.63.nc --tidewaterExists true --tidewater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_tides/fort.63.nc --tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/temp/ --backgroundChoice NAPATREE --graphDirectory ObsWater2023/
