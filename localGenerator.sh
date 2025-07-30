ba39edd388d06f8920ea2371ba6fe7eb59534d1e
Pranav Sai <pranavxd142@gmail.com>
2025-07-30 05:08:51 -0700

    Update scratch dir

--- localGenerator.sh
+++ localGenerator.sh
@@ -270,7 +270,7 @@ python generateGraphs.py --stations NAPATREE_NORMAL_STATIONS.json \
 --tidewaterExists true --tidewater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_tides/fort.63.nc \
 --meshExists true --mesh /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/fort.14 \
 --generateRunup true \
---tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/dec2023_runup_profile_temp/ --backgroundChoice NAPATREE --graphDirectory Runup2023Profile/
+--tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/dec2023_profile_temp/ --backgroundChoice NAPATREE --graphDirectory Runup2023Profile/
 
 # Water swath
 # python generateGraphs.py --stations OBS_STATIONS.json --obsExists true --waterExists true --water /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track/fort.63.nc --stillwaterExists true --stillwater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_wind/fort.63.nc --tidewaterExists true --tidewater /scratch3/workspace/pranav_sai_uri_edu-runup/Dec172023RunupRun/forecast_RI_track_tides/fort.63.nc --tempDir /scratch3/workspace/pranav_sai_uri_edu-runup/temp/ --backgroundChoice NAPATREE --graphDirectory ObsWater2023/
