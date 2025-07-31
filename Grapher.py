0b0b94108a2f9ba7f39fab48d9f27d9410b6c5cf
Pranav Sai <pranavxd142@gmail.com>
2025-07-31 09:20:29 -0700

    fix obs time indexing

--- Grapher.py
+++ Grapher.py
@@ -1029,7 +1029,7 @@ class Grapher:
                     datapointDuneHeights.append(runupDataset[stationKey]["duneHeights"][index])
 
                     
-                for index in range(len(runupDataset[stationKey]["swashHolmanHigh"][index])):
+                for index in range(len(runupDataset[stationKey]["swashHolmanHigh"])):
                     datapointHolmanHighSetup.append(self.unixTimeToDeltaHours(runupDataset[stationKey]["setupHolmanHigh"][index], self.runupStartDate))
                     datapointHolmanMidSetup.append(runupDataset[stationKey]["setupHolmanMid"][index])
                     datapointHolmanLowSetup.append(runupDataset[stationKey]["setupHolmanLow"][index])
