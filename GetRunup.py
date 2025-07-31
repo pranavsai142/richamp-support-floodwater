4ba501164306703902772c8fb87679345f7f3194
Pranav Sai <pranavxd142@gmail.com>
2025-07-30 23:55:07 -0700

    Add debugs

--- GetRunup.py
+++ GetRunup.py
@@ -884,8 +884,8 @@ class GetRunup:
                     base_dir="."
                 )
                 
-                for item in water_level_data[0]:
-                    print(water_level_data)
+                for item in water_level_data[0:5]:
+                    print(np.shape(water_level_data))
                     obsRunupTimes = item["unixTime"]
 #                     obsLatitude = item["siteLatitude"]
 #                     obsLongitude = item["siteLongitude"]
