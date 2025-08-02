3d4528d7809cd32fb32c0bf538ce5fb32103d199
Pranav Sai <pranavxd142@gmail.com>
2025-08-02 14:42:55 -0700

    fix typo

--- Grapher.py
+++ Grapher.py
@@ -1472,7 +1472,7 @@ class Grapher:
                         ALL_LONGITUDES_TRANSECTS.append(float(dc_data['lon'].iloc[0]))
                         ALL_LATITUDES_TRANSECTS.append(float(dc_data['lat'].iloc[0]))
                         with open("usgs_dune_crest_coordiantes.txt", "w") as f:
-                            for index in range(len(ALL_LONGITUDE_TRANSECTS)):
+                            for index in range(len(ALL_LONGITUDES_TRANSECTS)):
                                 f.write(str(ALL_LATITUDES_TRANSECTS[index]) + "," +  str(ALL_LONGITUDES_TRANSECTS[index]))
                         for twlccPoint in TWLCC_FORECAST_POINTS_IDENTIFIERS:
                             if state == twlccPoint[0] and segment == twlccPoint[1] and profile == twlccPoint[2]:
