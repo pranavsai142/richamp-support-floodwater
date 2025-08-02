e6b18a9514f5e8ff190e32149faecaf07d15e172
Pranav Sai <pranavxd142@gmail.com>
2025-08-02 15:11:24 -0700

    Fix marker

--- Grapher.py
+++ Grapher.py
@@ -1629,7 +1629,7 @@ class Grapher:
                     TWLCC_FORECAST_POINTS_LATITUDES[twlccIndex],
                     zorder=3,
                     alpha=0.7,
-                    marker="7",
+                    marker="CARETDOWN",
                     s=30,
                     color="green"
                 )
