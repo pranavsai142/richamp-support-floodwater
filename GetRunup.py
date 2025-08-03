8d2c3188dd2e9fa803491bd044a4dc96529ae27b
Pranav Sai <pranavxd142@gmail.com>
2025-08-03 09:50:35 -0700

    Update GetRunup.py

--- GetRunup.py
+++ GetRunup.py
@@ -636,7 +636,7 @@ class GetRunup:
                 else:
                     print("DID NOT FIND WATERLINE! Appending previous waterline key.")
                     print("If no previous key exists, will error out.")
-                    waterlineKeys.append(waterlineKey[-1])
+                    waterlineKeys.append(waterlineKeys[-1])
                     
 #                 Find the stillwater line, so we can pull SWL data without any gaps
                 for normalKey in normalDict:
