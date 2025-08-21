15c1b5c6deaec3f5a43a57f331c9a56920487a72
Pranav Sai <pranavxd142@gmail.com>
2025-08-20 17:33:27 -0700

    Graph east coast

--- Grapher.py
+++ Grapher.py
@@ -1917,7 +1917,7 @@ class Grapher:
         if(len(self.mapWaveTimes) > 0):
             vmin = 0
             vmax = math.ceil(self.maxSWH)
-            vmax = 10
+            vmax = 17
             levels = 100
             levelBoundaries = np.linspace(vmin, vmax, levels + 1)
         
